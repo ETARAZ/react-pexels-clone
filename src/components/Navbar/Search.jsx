a488dded059f15c8c0fb7f21d1df18931341ac1c
Marouane Etaraz <54443574+ETARAZ@users.noreply.github.com>
2021-12-09 01:17:23 +0100

    chng

--- src/components/Navbar/Search.jsx
+++ src/components/Navbar/Search.jsx
@@ -10,7 +10,7 @@ function Search({ text,hero }) {
         setFocus(false)
     }
     return (
-        <div className="max-w-750 group  w-full mr-4">
+        <div className="max-w-750 group z-20  w-full mr-4">
             <form className="relative z-10">
                 <div className={`z-10 relative flex ${focus ? 'bg-white rounded-br-none rounded-bl-none  drop-shadow-lg' : "bg-back"}  transition-colors   rounded-md`}>
                     <input onBlur={handleBlur} onFocus={handleFocus} type="search" placeholder={text} className={` flex-initial bg-transparent  text-[#1a1a1a] p-3 sm:px-5 ${hero ? "py-5" : "sm:py-3"} not-italic border-0 outline-none placeholder-size placeholder-[#5e5e5e]     text-17 leading-22 w-full font-semibold sm:font-normal `} />
